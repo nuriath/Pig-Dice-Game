9245022c0ccddbafff10079ce600132c224b50f3
Mwangaza Nuriath <mnuriath28@gmail.com>
2019-01-11 14:25:17 +0200

    put the prototype in js file

--- js/script.js
+++ js/script.js
@@ -14,3 +14,21 @@ function Player (turn) {
     this.playerName;
 }
 
+// checking for 1
+Player.prototype.rollone = function() {
+    if (this.roll===1) {
+        this.tempscore = 0;
+        alert("Sorry " + this.playerName + ", you rolled a 1! your turn is over!")
+// this.changeturn();
+  } else {
+        this.tempscore += this.roll;
+    }
+}
+
+//hold 
+Player.prototype.hold = function() {
+    this.totalscore += this.tempscore;
+    this.tempscore = 0;
+    //this.changeturn();
+    alert(this.playerName + ", your turn is over, pass the mouse!");
+}
